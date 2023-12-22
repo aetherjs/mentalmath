b6a8876b5b1e718079a1a780cd56fe88e4759e1e
Vlad Repinskiy <vladislav.repinskiy@zuehlke.com>
2023-12-22 12:15:07 +0100

    Remove unused assets, set up eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,14 @@ const App: React.FC = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-white px-4">
       <h1 className="text-2xl md:text-4xl font-bold mb-8 text-black">Mental Math Practice</h1>
       <div className="flex flex-wrap justify-around w-full max-w-2xl space-y-4 md:space-y-0">
-        <button 
+        <button
           className="flex items-center justify-center w-32 h-32 p-2 border border-black text-black font-bold rounded-lg focus:outline-none disabled:opacity-50"
-          disabled
-        >
-          Timed Mode (Coming Soon)
+          disabled>
+          Timed Mode (Coming Soon) Bebe
         </button>
-        <button 
+        <button
           className="flex items-center justify-center w-32 h-32 p-2 border border-black text-black font-bold rounded-lg focus:outline-none"
-          onClick={() => alert('Navigate to Endless Mode')}
-        >
+          onClick={() => alert('Navigate to Endless Mode')}>
           Endless Casual Mode
         </button>
       </div>
@@ -23,4 +21,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
